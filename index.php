19f255eb230d2af0bfe74d726cfecfdf56d1b0fd
sharan-govankop <2ke19cs092@kleit.ac.in>
2023-04-15 18:38:32 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Online Quiz System |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/image.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/quiz.jpeg) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1 style="color:purple;"> online quiz system </h1>
+                <a href="login.php" class="btn" style="color:blue;"> login </a> &emsp;
+                <a href="register.php" class="btn" style="color:blue"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
